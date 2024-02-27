dd214df9db8e98a5c4830c1ffe920db79abbb7c6
Avinash <avin.roy001@gmail.com>
2024-02-27 23:44:11 +0530

    done commit

--- xdisplayname/src/App.jsx
+++ xdisplayname/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [flag,setFlag] = useState(false)
+  function fullName(e){
+    e.preventDefault();
+    setFlag(true);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form onSubmit={fullName}>
+        <h1>Full Name Display</h1>
+        <br/>
+        <div>
+        <label>First Name:<input type='text' onChange={(e)=>setFirst(e.target.value)} required/></label>
+        </div>
+       <div>
+       <label>Last Name:<input type='text' onChange={(e)=>setLast(e.target.value)} required/></label>
+       </div>
+        <button type='submit'>Submit</button>
+    </form>
+    {flag?(<p>Full Name:{first} {last}</p>):""}
     </>
   )
 }
